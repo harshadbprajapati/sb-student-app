28a7f0eeb796f2c077f6ae3343fd4e7e07e2b108
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-01-27 16:21:08 +0530

    add custom user details service

---
+++ src/main/java/org/example/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package org.example.repository;
+
+import org.example.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
+
+
--- src/main/java/org/example/security/AppSecurityConfiguration.java
+++ src/main/java/org/example/security/AppSecurityConfiguration.java
@@ -1,31 +1,54 @@
 package org.example.security;
 
+import org.example.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
 
 @Configuration
 public class AppSecurityConfiguration {
+    private CustomUserDetailsService customUserDetailsService;
+    @Autowired
+    public AppSecurityConfiguration(CustomUserDetailsService userDetailsService) {
+        this.customUserDetailsService = userDetailsService;
+    }
 
     @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource){
-        JdbcUserDetailsManager userDetailsManager =
-                new JdbcUserDetailsManager(dataSource);
-        userDetailsManager.setUsersByUsernameQuery(
-                "select membername, passwd, active from members where membername=?"
-        );
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration
+    ) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
-        userDetailsManager.setAuthoritiesByUsernameQuery(
-                "select membername, role from roles where membername=?"
-        );
-        return userDetailsManager;
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return customUserDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(customUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
     @Bean
